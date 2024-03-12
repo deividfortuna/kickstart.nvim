3b7174df2a02a7f4acac95c9b1ec63a11b71315b
Deivid Fortuna <deividfortuna@gmail.com>
2024-03-12 17:03:54 +1300

    more remaps

--- lua/deivid/remap.lua
+++ lua/deivid/remap.lua
@@ -2,14 +2,23 @@
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
 
 -- Set jk to escape from insert mode
-vim.keymap.set('i', 'jk', '<Esc>', { noremap = true, silent = true, expr = false })
+vim.keymap.set('i', 'jk', '<Esc>')
+-- Set Ctrl + c to escape from insert mode
+vim.keymap.set('i', '<C-c>', '<Esc>')
 
 -- Move lines up and down
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
 
+-- Move page up and down but keep cursor position
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+
 -- Open terminal mode in a split on the bottom
 vim.keymap.set('n', '<leader>tt', '<cmd>below 10sp +term<CR>', { desc = 'Open [T]erminal' })
+
+-- Replace selected text with a provided value
+vim.keymap.set('n', '<leader>rr', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = '[R]eplace Word' })
 
 -- Format buffer
 vim.keymap.set('n', '<leader>f', vim.lsp.buf.format, { desc = '[F]ormat buffer' })
